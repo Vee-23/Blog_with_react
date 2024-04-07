f8bcda8fbc59a34895ffe7d0b94900d56795fa03
Vee-23 <vinaysaldanha101@gmail.com>
2024-04-07 12:57:37 +0530

    configured the RTK-store and installed tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './Appwrite/auth'
+import {login, logOut} from './store/authSlice'
+import {Header, Footer} from './components'
+import './index.css'
 
 function App() {
 
-  return (
-    <>
-     <div>Testing...</div>
-    </>
-  )
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch()
+  useEffect(() => {
+      authService.getCurrentUser()
+      .then((userData) => {
+          if(userData){
+              dispatch(login({userData}))
+          }
+          else{
+              dispatch(logOut())
+          }
+      })
+      .finally(()=> setloading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='text-white min-h-screen flex flex-wrap content-between p-2 bg-slate-800'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* outlet */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
