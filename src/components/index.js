787ea6c9449cd7da573e8d3081111a930bbfbc33
Vee-23 <vinaysaldanha101@gmail.com>
2024-04-09 12:38:01 +0530

    added blog post ading component with functionality

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./Footer/Footer";
 import Container from "./Container";
 import Logo from "./Logo";
 import LogOutBtn from "./Header/LogOutBtn";
+import RTE from "./RTE";
 
 export {
         Header,
         Footer,
         Container,
         Logo,
-        LogOutBtn
+        LogOutBtn,
+         RTE
 }
